577f92362218d885708d2cabb9f45f50380e2289
josh-257 <joshbenton05@gmail.com>
2026-08-12 22:15:05 +0100

    Feat: Implemented GPIO driver

--- Drivers/Inc/Driver_GPIO.h
+++ Drivers/Inc/Driver_GPIO.h
@@ -9,27 +9,149 @@
 #define INC_DRIVER_GPIO_H_
 
 #include <stdint.h>
+#include "mcu.h"
 
 //Configuration structure for GPIO pin
 typedef struct{
-  uint8_t GPIO_PinNumber;
-  uint8_t GPIO_PinMode;
-  uint8_t GPIO_PinSpeed;
-  uint8_t GPIO_PinPuPdControl;
-  uint8_t GPIO_PinOPType;
-  uint8_t GPIO_AltFunMode;
+  uint8_t PinNumber;
+  uint8_t PinMode;
+  uint8_t PinSpeed;
+  uint8_t PinPuPdControl;
+  uint8_t PinOPType;
+  uint8_t AltFunMode;
 }GPIO_PinConfig_t;
 
-//Handle structure for GPIO pin
-typedef struct{
-  GPIO_RegDef_t *pGPIOx;
-  GPIO_PinConfig_t GPIO_PinConfig;
-}GPIO_Handle_t;
+//Pin modes
+#define GPIO_MODE_IN          0
+#define GPIO_MODE_OUT         1
+#define GPIO_MODE_ALTFN       2
+#define GPIO_MODE_ANALOG      3
 
-void GPIO_Init();
+//Macros to reset GPIOx peripherals
+#define GPIOA_REG_RESET()     do{(RCC->AHB1RSTR |= (1 << 0)); (RCC->AHB1RSTR &= ~(1 << 0));} while(0)
+#define GPIOB_REG_RESET()     do{(RCC->AHB1RSTR |= (1 << 1)); (RCC->AHB1RSTR &= ~(1 << 1));} while(0)
+#define GPIOC_REG_RESET()     do{(RCC->AHB1RSTR |= (1 << 2)); (RCC->AHB1RSTR &= ~(1 << 2));} while(0)
+#define GPIOD_REG_RESET()     do{(RCC->AHB1RSTR |= (1 << 3)); (RCC->AHB1RSTR &= ~(1 << 3));} while(0)
+#define GPIOE_REG_RESET()     do{(RCC->AHB1RSTR |= (1 << 4)); (RCC->AHB1RSTR &= ~(1 << 4));} while(0)
+#define GPIOF_REG_RESET()     do{(RCC->AHB1RSTR |= (1 << 5)); (RCC->AHB1RSTR &= ~(1 << 5));} while(0)
+#define GPIOG_REG_RESET()     do{(RCC->AHB1RSTR |= (1 << 6)); (RCC->AHB1RSTR &= ~(1 << 6));} while(0)
+#define GPIOH_REG_RESET()     do{(RCC->AHB1RSTR |= (1 << 7)); (RCC->AHB1RSTR &= ~(1 << 7));} while(0)
+#define GPIOI_REG_RESET()     do{(RCC->AHB1RSTR |= (1 << 8)); (RCC->AHB1RSTR &= ~(1 << 8));} while(0)
 
-void GPIO_DeInit();
+//Clock disable macros for GPIOx peripherals
+#define GPIOA_PCLK_DI()       (RCC->AHB1ENR &= ~(1 << 0))
+#define GPIOB_PCLK_DI()       (RCC->AHB1ENR &= ~(1 << 1))
+#define GPIOC_PCLK_DI()       (RCC->AHB1ENR &= ~(1 << 2))
+#define GPIOD_PCLK_DI()       (RCC->AHB1ENR &= ~(1 << 3))
+#define GPIOE_PCLK_DI()       (RCC->AHB1ENR &= ~(1 << 4))
+#define GPIOF_PCLK_DI()       (RCC->AHB1ENR &= ~(1 << 5))
+#define GPIOG_PCLK_DI()       (RCC->AHB1ENR &= ~(1 << 6))
+#define GPIOH_PCLK_DI()       (RCC->AHB1ENR &= ~(1 << 7))
+#define GPIOI_PCLK_DI()       (RCC->AHB1ENR &= ~(1 << 8))
 
+//Clock enable macros for GPIOx peripherals
+//Dummy read added to get around RCC peripheral delay (STM32F4 Errata)
+#define GPIOA_PCLK_EN()       do{(RCC->AHB1ENR |= (1 << RCC_AHB1ENR_GPIOAEN_Pos));\
+															volatile uint32_t dummy = RCC->AHB1ENR;\
+															(void)dummy;}while(0)
+
+#define GPIOB_PCLK_EN()       do{(RCC->AHB1ENR |= (1 << RCC_AHB1ENR_GPIOBEN_Pos));\
+															volatile uint32_t dummy = RCC->AHB1ENR;\
+															(void)dummy;}while(0)
+
+#define GPIOC_PCLK_EN()       do{(RCC->AHB1ENR |= (1 << RCC_AHB1ENR_GPIOCEN_Pos));\
+															volatile uint32_t dummy = RCC->AHB1ENR;\
+															(void)dummy;}while(0)
+
+#define GPIOD_PCLK_EN()       do{(RCC->AHB1ENR |= (1 << RCC_AHB1ENR_GPIODEN_Pos));\
+															volatile uint32_t dummy = RCC->AHB1ENR;\
+															(void)dummy;}while(0)
+
+#define GPIOE_PCLK_EN()       do{(RCC->AHB1ENR |= (1 << RCC_AHB1ENR_GPIOEEN_Pos));\
+															volatile uint32_t dummy = RCC->AHB1ENR;\
+															(void)dummy;}while(0)
+
+#define GPIOF_PCLK_EN()       do{(RCC->AHB1ENR |= (1 << RCC_AHB1ENR_GPIOFEN_Pos));\
+															volatile uint32_t dummy = RCC->AHB1ENR;\
+															(void)dummy;}while(0)
+
+#define GPIOG_PCLK_EN()       do{(RCC->AHB1ENR |= (1 << RCC_AHB1ENR_GPIOGEN_Pos));\
+															volatile uint32_t dummy = RCC->AHB1ENR;\
+															(void)dummy;}while(0)
+
+#define GPIOH_PCLK_EN()       do{(RCC->AHB1ENR |= (1 << RCC_AHB1ENR_GPIOHEN_Pos));\
+															volatile uint32_t dummy = RCC->AHB1ENR;\
+															(void)dummy;}while(0)
+
+#define GPIOI_PCLK_EN()       do{(RCC->AHB1ENR |= (1 << RCC_AHB1ENR_GPIOIEN_Pos));\
+															volatile uint32_t dummy = RCC->AHB1ENR;\
+															(void)dummy;}while(0)
+
+
+/*******************************API's supported by this driver*******************************/
+
+/***************************************************************************
+ * @brief   Configures GPIO pin with user-defined GPIO handle structure.
+ * @param   pGPIOHandle: Pointer to handle structure containing user-defined
+ *          configuration.
+ */
+void GPIO_Init(GPIO_TypeDef *pGPIOx, const GPIO_PinConfig_t *pGPIOConfig);
+
+/***************************************************************************
+ * @brief   Resets all registers in corresponding GPIO port.
+ * @param   pGPIOHandle: Pointer to handle structure containing user-defined
+ *          configuration.
+ * @note    This function resets the physical port but not the handle
+ *          structure.
+ */
+void GPIO_DeInit(GPIO_TypeDef *pGPIOx);
+
+/***************************************************************************
+ * @brief   Returns the current value held in the input data register of
+ *          the specified GPIO pin.
+ * @param   pGPIOx: Pointer to register definition structure
+ *          containing memory address of chosen GPIO port.
+ * @param   PinNumber: GPIO_PIN_NO_0 - GPIO_PIN_NO_15.
+ * @retval  The value of the input data register.
+ */
+uint8_t GPIO_ReadFromInputPin(GPIO_TypeDef *pGPIOx, uint8_t PinNumber);
+
+/***************************************************************************
+ * @brief   Returns the current values held in the input data register of
+ *          the specified GPIO port.
+ * @param   pGPIOx: Pointer to register definition structure
+ *          containing memory address of chosen GPIO port.
+ * @retval  The value of the input data register across
+ *          all pins for specific port.
+ */
+uint16_t GPIO_ReadFromInputPort(GPIO_TypeDef *pGPIOx);
+
+/***************************************************************************
+ * @brief   Writes specified value (either SET or RESET) to chosen GPIO pin.
+ * @param   pGPIOx: Pointer to register definition structure
+ *          containing memory address of chosen GPIO port.
+ * @param   PinNumber: GPIO_PIN_NO_0 - GPIO_PIN_NO_15.
+ * @param   value: Either SET(1) or RESET(0).
+ */
+void GPIO_WriteToOutputPin(GPIO_TypeDef *pGPIOx, uint8_t PinNumber, uint8_t value);
+
+/***************************************************************************
+ * @brief   Writes specified 16 bit value to set and reset
+ *          register of chosen GPIO port.
+ * @param   pGPIOx: Pointer to register definition structure
+ *          containing memory address of chosen GPIO port.
+ * @param   value: Holding set values (0-8 bits) and
+ *          reset values (bits 8-15) for each pin of the GPIO port.
+ */
+void GPIO_WriteToOutputPort(GPIO_TypeDef *pGPIOx, uint16_t value);
+
+/***************************************************************************
+ * @brief   Toggles state of specified GPIO pin.
+ * @param   pGPIOx: Pointer to register definition structure
+ *          containing memory address of chosen GPIO port.
+ * @param   PinNumber: GPIO_PIN_NO_0 - GPIO_PIN_NO_15.
+ */
+void GPIO_ToggleOutputPin(GPIO_TypeDef *pGPIOx, uint8_t PinNumber);
 
 
 #endif /* INC_DRIVER_GPIO_H_ */
